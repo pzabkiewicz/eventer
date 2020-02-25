865abf71c0bf4a09c193bc6b93484c6a607a7023
Patryk Zabkiewicz <patryk.zabkiewicz@capgemini.com>
2020-02-25 22:03:14 +0100

    Service for events and model of events for saving form inputs created

---
+++ src/app/model/event-save-model.ts
@@ -0,0 +1,24 @@
+export class DiscountsSaveModel {
+    
+    constructor(
+        id: string,
+        value: boolean
+    ) {}
+
+}
+
+export class EventSaveModel {
+    
+    constructor(
+        public name: string,
+        public location: string,
+        public date: Date,
+        public hour: Date,
+        public price: string,
+        public places: number, 
+        public imgPath: string,
+        public description: string,
+        public discounts: Array<DiscountsSaveModel>
+    ) {}
+
+}
